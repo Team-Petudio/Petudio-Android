ea8734372de462d0d8234bd8f636959a8bcb9128
buna <tmdgh1592@naver.com>
2024-01-12 15:54:14 +0900

    chore: glide:compose:1.0.0-beta01 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,4 +74,5 @@ dependencies {
     implementation("androidx.compose.ui:ui:1.5.4")
     implementation("androidx.compose.material:material:1.5.4")
     implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("com.github.bumptech.glide:compose:1.0.0-beta01")
 }
