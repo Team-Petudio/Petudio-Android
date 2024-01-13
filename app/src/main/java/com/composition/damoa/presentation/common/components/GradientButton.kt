1726240cdf74e28f1291fdd44cbb939753d15844
buna <tmdgh1592@naver.com>
2024-01-13 10:35:44 +0900

    feat: GradientButton에 enabled 속성 추가

--- app/src/main/java/com/composition/damoa/presentation/common/components/GradientButton.kt
+++ app/src/main/java/com/composition/damoa/presentation/common/components/GradientButton.kt
@@ -3,7 +3,10 @@ package com.composition.damoa.presentation.common.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -13,8 +16,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.composition.damoa.R
+import com.composition.damoa.presentation.ui.theme.Gray60
+import com.composition.damoa.presentation.ui.theme.PrimaryColors
 
 @Composable
 fun GradientButton(
@@ -25,6 +35,7 @@ fun GradientButton(
     fontSize: TextUnit = TextUnit.Unspecified,
     fontWeight: FontWeight? = null,
     gradient: Brush,
+    enabled: Boolean = true,
     onClick: () -> Unit,
 ) {
     Button(
@@ -35,13 +46,17 @@ fun GradientButton(
             ),
         contentPadding = PaddingValues(),
         shape = shape,
+        enabled = enabled,
         onClick = onClick,
     ) {
+        val boxModifier =
+            if (enabled) {
+                Modifier.background(gradient)
+            } else {
+                Modifier.background(Gray60)
+            }
         Box(
-            modifier =
-                Modifier
-                    .background(gradient)
-                    .fillMaxSize(),
+            modifier = boxModifier.fillMaxSize(),
             contentAlignment = Alignment.Center,
         ) {
             Text(
@@ -52,4 +67,23 @@ fun GradientButton(
             )
         }
     }
+}
+
+@Preview
+@Composable
+private fun GradientButtonPreview() {
+    GradientButton(
+        modifier =
+            Modifier
+                .fillMaxWidth()
+                .aspectRatio(6 / 1F),
+        shape = RoundedCornerShape(12.dp),
+        text = stringResource(id = R.string.keep_going),
+        fontColor = Color.White,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Bold,
+        gradient = Brush.horizontalGradient(PrimaryColors),
+        onClick = { },
+        enabled = false,
+    )
 }
