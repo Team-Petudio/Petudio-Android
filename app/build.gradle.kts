9c8e3c252f4295e612cc1e2485ea06dc9b93d299
buna <tmdgh1592@naver.com>
2024-01-25 22:00:53 +0900

    chore: billing(구글 결제) 6.1.0 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -132,6 +132,9 @@ dependencies {
     // Hilt
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-android-compiler:2.50")
+
+    // Billing
+    implementation("com.android.billingclient:billing:6.1.0")
 }
 
 kapt {
