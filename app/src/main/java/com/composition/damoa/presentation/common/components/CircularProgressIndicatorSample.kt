b873b28be314d0536ae8036e4adab679abae6b0a
buna <tmdgh1592@naver.com>
2024-01-28 18:44:44 +0900

    feat: CircularLoadingBar 구현

---
+++ app/src/main/java/com/composition/damoa/presentation/common/components/CircularProgressIndicatorSample.kt
@@ -0,0 +1,18 @@
+package com.composition.damoa.presentation.common.components
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.composition.damoa.presentation.ui.theme.Purple60
+
+@Composable
+fun CircularLoadingBar(size: Dp = 50.dp) {
+    CircularProgressIndicator(
+        modifier = Modifier.size(size),
+        color = Purple60,
+        strokeWidth = 5.dp,
+    )
+}
