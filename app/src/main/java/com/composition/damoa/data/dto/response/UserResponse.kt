01d17e509b4772c79035d86f8e364ff33964d3cc
buna <tmdgh1592@naver.com>
2024-02-09 14:21:20 +0900

    fix: 로그인 안 되는 버그 수정(UserResponse의 userId를 memberId로 변경)

--- app/src/main/java/com/composition/damoa/data/dto/response/UserResponse.kt
+++ app/src/main/java/com/composition/damoa/data/dto/response/UserResponse.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class UserResponse(
-    @SerialName("userId")
+    @SerialName("memberId")
     val id: Long,
     @SerialName("socialType")
     val socialType: SocialType,
