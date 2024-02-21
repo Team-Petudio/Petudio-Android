d05d851a64651a117b14e81ebfae8388c4cdcd52
buna <tmdgh1592@naver.com>
2024-02-21 17:52:52 +0900

    fix(PetService): addPet() 메서드 인자에 @Body 애노테이션 추가

--- app/src/main/java/com/composition/damoa/data/service/PetService.kt
+++ app/src/main/java/com/composition/damoa/data/service/PetService.kt
@@ -4,6 +4,7 @@ import com.composition.damoa.data.common.retrofit.callAdapter.ApiResponse
 import com.composition.damoa.data.dto.request.PetRequest
 import com.composition.damoa.data.dto.response.BaseResponse
 import com.composition.damoa.data.dto.response.PetsResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 
@@ -12,5 +13,7 @@ interface PetService {
     suspend fun getPets(): ApiResponse<BaseResponse<PetsResponse>>
 
     @POST("/api/v1/pet/add")
-    suspend fun addPet(request: PetRequest): ApiResponse<Unit>
+    suspend fun addPet(
+        @Body request: PetRequest,
+    ): ApiResponse<Unit>
 }
