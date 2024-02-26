a6f9eb0266c5a1cadf776f9b5204b9b199a80639
buna <tmdgh1592@naver.com>
2024-02-26 16:10:59 +0900

    chore: KotlinCompile 보고서 옵션을 build.gradle.kts(:app) 으로 이동
    - project 레벨에서는 동작 안 함

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,24 +6,12 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10" apply false
     id("com.google.dagger.hilt.android") version "2.44" apply false
     id("com.android.test") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.kapt") version "1.8.10" apply false
 }
 
 subprojects {
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         kotlinOptions {
-            if (project.findProperty("composeCompilerReports") == "true") {
-                freeCompilerArgs += listOf(
-                    "-P",
-                    "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=${project.buildDir.absolutePath}/compose_compiler"
-                )
-            }
-            if (project.findProperty("composeCompilerMetrics") == "true") {
-                freeCompilerArgs += listOf(
-                    "-P",
-                    "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=${project.buildDir.absolutePath}/compose_compiler"
-                )
-            }
+
         }
     }
 }
