63c4453da1993731f8a5fcf0284cc912ffafae7e
buna <tmdgh1592@naver.com>
2024-02-27 23:21:18 +0900

    refactor: 관련 있는 의존성들끼리 모여 있도록 라인 포맷팅

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,6 +107,7 @@ fun getLocalPropertyValue(propertyKey: String): String {
 }
 
 dependencies {
+    // Android
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -114,14 +115,22 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.6")
     implementation("androidx.tv:tv-material:1.0.0-alpha10")
-    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
     testImplementation("junit:junit:4.13.2")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
 
+    // Google
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
+    // Kakao
     implementation("com.kakao.sdk:v2-user:2.19.0")
-    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+
+    // Firebase
+    val firebaseBomVersion = "32.7.1"
+
+    implementation(platform("com.google.firebase:firebase-bom:$firebaseBomVersion"))
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-messaging-ktx")
     implementation("com.google.firebase:firebase-auth")
