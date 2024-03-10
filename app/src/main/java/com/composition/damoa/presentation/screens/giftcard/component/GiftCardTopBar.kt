5ea793ab9a24b0f7a91c48d2b2e319caeae19c61
buna <tmdgh1592@naver.com>
2024-03-10 22:41:01 +0900

    fix(GiftCard): 뒤로가기 버튼을 Left Arrow로 변경
    - 기존에 RightArrow로 되어 있는 버그

--- app/src/main/java/com/composition/damoa/presentation/screens/giftcard/component/GiftCardTopBar.kt
+++ app/src/main/java/com/composition/damoa/presentation/screens/giftcard/component/GiftCardTopBar.kt
@@ -3,6 +3,7 @@ package com.composition.damoa.presentation.screens.giftcard.component
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBar
@@ -23,7 +24,7 @@ fun GiftCardTopBar(
         navigationIcon = {
             IconButton(onClick = onNavigationClick) {
                 Icon(
-                    imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                    imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
                     contentDescription = stringResource(id = R.string.navigate_back),
                     tint = Color.Black,
                 )
